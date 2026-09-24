3a5ea52d5a77ecb4ecb785de0dbf9de6e50ad62f
Louis Goerlt <louis.goerlt@gmail.com>
2026-09-24 10:33:36 +0200

    fix solver

--- main.pl
+++ main.pl
@@ -1,14 +1,21 @@
 :- use_module(library(clpfd)).
 
 sudoku(S) :-
+    % general layout
     length(S, 9),
+    maplist(same_length(S), S),
     maplist(valid_sodoku_nums, S),
+    append(S, Vs),
+    Vs ins 1..9,
+    % content requirements
     transpose(S, T),
     maplist(valid_sodoku_nums, T),
     [R1, R2, R3, R4, R5, R6, R7, R8, R9] = S,
     valid_boxes(R1, R2, R3),
     valid_boxes(R4, R5, R6),
-    valid_boxes(R7, R8, R9).
+    valid_boxes(R7, R8, R9),
+    % labeling
+    labeling([ff], Vs).
 
 valid_boxes([], [], []).
 valid_boxes([A, B, C | R1], [D, E, F | R2], [G, H, I | R3]) :-
@@ -17,15 +24,4 @@ valid_boxes([A, B, C | R1], [D, E, F | R2], [G, H, I | R3]) :-
 
 valid_sodoku_nums(L) :-
     length(L, 9),
-    maplist(digit, L),
     all_distinct(L).
-
-digit(1).
-digit(2).
-digit(3).
-digit(4).
-digit(5).
-digit(6).
-digit(7).
-digit(8).
-digit(9).
